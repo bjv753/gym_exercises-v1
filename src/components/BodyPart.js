059a8574b53b9dbdd0e551e7b1e06470deb4c3ff
Benjamin Vredevelt <bjv753@gmail.com>
2023-10-11 13:38:15 -0500

    update

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import { Stack, Typography } from '@mui/material';
 
-const BodyPart = () => {
+import Icon from '../assets/icons/gym.png'
+const BodyPart = ({ item, bodyPart, setBodyPart }) => {
   return (
-    <div>BodyPart</div>
+    <Stack
+      type="button"
+      alignItems="center"
+      justifyContent="center"
+      className="bodyPart-card"
+      sx={
+        bodyPart === item ? {
+          borderTop: '4px solid #ff2625',
+          backgroundColor: '#fff',
+          borderBottomLeftRadius: '20px',
+          width: '270px',
+          height: '280px',
+          cursor: 'pointer',
+          gap: '47px'
+        } : {
+          background: '#fff',
+          borderBottomLeftRadius: '20px',
+          width: '270px',
+          height: '280px',
+          cursor: 'pointer',
+          gap: '47px'
+        }
+      }
+    >
+      <img  src={Icon} alt="dumbbell" styles={{ width: '40px', height: '40px' }} />
+    </Stack>
   )
 }
 
